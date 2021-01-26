81a632c839e5d07883a3b367104f635bc5814205
Dathan Rivera <dwrivera7@gmail.com>
2021-01-25 20:00:01 -0600

    Finished all 12 tests.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,82 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
+    //Test 1
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void emptyStringTest(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
     }
 
+    //Test 2
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    //Test 3
+    @Test
+    public void backwardsBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    //Test 4
+    @Test
+    public void threeBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][["));
+    }
+
+    //TTest 5
+    @Test
+    public void twoPairsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    //Test 6
+    @Test
+    public void dungeonsAndDragonsCharacterWithBracketsIsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Ironsight]"));
+    }
+
+    //Test 7
+    @Test
+    public void dungeonsAndDragonsCharacterWithBackwardsBracketsIsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Ironsight["));
+    }
+
+    //Test 8
+    @Test
+    public void threeOpensOneCloseReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]"));
+    }
+
+    //Test 9
+    @Test
+    public void threeCloseOneOpenReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]["));
+    }
+
+    //Test 10
+    @Test
+    public void witcherFortressWithInsertedBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Kaer][Morhen]"));
+    }
+
+    //Test 11
+    @Test
+    public void witcherFortressWithBackwardsBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Kaer[ ]Morhen["));
+    }
+
+    //Test 12
+    @Test
+    public void witcherFortressWithBracketsAroundSecondWordReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Kaer [Morhen]"));
+    }
 
 }
